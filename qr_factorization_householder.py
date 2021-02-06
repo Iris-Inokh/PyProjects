3f9d6dd482c952beceabb2ac5425d24f7eb9ad67
IrisInokh <irisinokhosa@gmail.com>
2021-02-06 10:58:19 +0100

    qr matrices check added

--- qr_factorization_householder.py
+++ qr_factorization_householder.py
@@ -26,21 +26,21 @@ class QR:
         #m - number of dimentions
         self.Q_final = np.identity(self.m)
         for i in range(self.n):
-            a_norm = np.linalg.norm(np.transpose(self.R)[i][i:])       #norm of a given vector
+            a_norm = np.linalg.norm(np.transpose(self.R)[i][i:])    #norm of a given vector
             e1 = np.zeros(self.m-i) 
-            e1[0] = 1.                                          #construct basis vector e1 
-            vec_1 = np.transpose(self.R)[i][i:]                      #a1 vector in matrix A    
-            vec_1.shape = (np.size(vec_1), 1)                   #reshape to (, 1)
-            vec_2 = np.sign(vec_1[0][0])*a_norm * e1            #vector with shape (m-i, )
-            vec_2.shape = (np.size(vec_2), 1)                   #reshape to (, 1)
+            e1[0] = 1.                                              #construct basis vector e1 
+            vec_1 = np.transpose(self.R)[i][i:]                     #a1 vector in matrix A    
+            vec_1.shape = (np.size(vec_1), 1)                       #reshape to (, 1)
+            vec_2 = np.sign(vec_1[0][0])*a_norm * e1                #vector with shape (m-i, )
+            vec_2.shape = (np.size(vec_2), 1)                       #reshape to (, 1)
             v1 = np.add(vec_1, vec_2)     
 
-            prod_vec = np.outer(np.transpose(v1),v1)            #v1 * v1t -> matrix
-            prod_vec2 = np.transpose(v1).dot(v1)                #1d array
+            prod_vec = np.outer(np.transpose(v1),v1)                #v1 * v1t -> matrix
+            prod_vec2 = np.transpose(v1).dot(v1)                    #1d array
             k = 2/prod_vec2
-            Q1 = np.subtract(np.identity(self.m-i), k*prod_vec)      #reflexion
+            Q1 = np.subtract(np.identity(self.m-i), k*prod_vec)     #reflexion
             Q_full = np.identity(self.m)
-            Q_full[i:Q1.shape[0]+i, i:Q1.shape[1]+i] = Q1       #expand Q matrix to m dimentions with identity matrix
+            Q_full[i:Q1.shape[0]+i, i:Q1.shape[1]+i] = Q1           #expand Q matrix to m dimentions with identity matrix
             self.Q_final = self.Q_final.dot(Q_full)
             self.R = Q_full.dot(self.R)
     
@@ -48,41 +48,32 @@ class QR:
 
 
 if __name__ == "__main__":
-    # Matrix aus der Uebung:
+    # test Matrix 01:
     A = np.array([ \
             [-2, -2, -2], \
             [-2, -1, -1], \
             [1, 0, -1]] \
             , dtype=float)
 
-    #test Matrix from HA4.1
-    test = np.array([ \
+    #test Matrix 02
+    B = np.array([ \
             [1, -2, -1], \
             [2, 0, 1], \
             [2, -4, 2], \
             [4, 0, 0] ]\
             ,dtype=float)  
-    my_qr = QR()
-    Q, R = my_qr.get_decomp(test)
+    
+    my_qr = QR()                    #create class instance
+    Q, R = my_qr.get_decomp(A)      #compute decomposition
+    #Q, R = my_qr.get_decomp(B)
+    
+    ### Check Q and R matricies
+    q_test = np.allclose(Q.T@Q, np.identity(Q.shape[0]))
+    print(f"Q orthogonal? -> {q_test}")
+    r_test = np.allclose(R, np.triu(R))             #returns upper triangle of array
+    print(f"R upper triangilar? -> {r_test}")
+    a_test = np.allclose(A, Q@R)
+    print(f"Q*R = A? -> {a_test}")
+    # b_test = np.allclose(B, Q@R)
+    # print(f"Q*R = A? -> {b_test}")
 
-    """
-    print("R[i,i] > 0?")
-    Rdiag = np.diag(R)                          # Extrahieren der Hauptdiagonalen
-    print(np.all(Rdiag > 0.))
-    print('')
-
-    print("R[i,j] = 0 falls i > j?")
-    #is_uppertri = np.all(np.triu(R) == R)      Gleichheit zu ueberpruefen geht aufgrund von numerischen fast immer schief
-    is_uppertri = np.allclose(R, np.triu(R))    # Wir pruefen "numerische Gleichheit"
-    print(is_uppertri)
-    print('')
-
-    print("QR = A?")
-    print(np.allclose(Q@R, A))
-    print('')
-
-    print("Q.T*Q = I?")
-    print(np.allclose(Q.T@Q, np.identity(Q.shape[0])))
-    print('')
-
-    """
